1451727ba9a5dffc9d7caeee0fd1ad844f3876c4
jayree <jayree.git@icloud.com>
2021-04-23 18:03:49 +0200

    fix: plugin uninstall

--- src/hooks/prerun.ts
+++ src/hooks/prerun.ts
@@ -14,31 +14,33 @@ const isContentTypeJSON = env.getString('SFDX_CONTENT_TYPE', '').toUpperCase() =
 const isOutputEnabled = !(process.argv.find((arg) => ['--json', '--help', '-h'].includes(arg)) || isContentTypeJSON);
 
 export const prerun: Hook<'prerun'> = async function (options) {
-  env.setBoolean('OCLIF_DISABLE_UPDATENOTIFIER', true);
-  debug('set: OCLIF_DISABLE_UPDATENOTIFIER=true');
-  if (isOutputEnabled) {
-    if (['plugins:update', 'plugins:update:check', 'update'].includes(options.Command.id)) {
+  if (!['plugins:install', 'plugins:uninstall'].includes(options.Command.id)) {
+    env.setBoolean('OCLIF_DISABLE_UPDATENOTIFIER', true);
+    debug('set: OCLIF_DISABLE_UPDATENOTIFIER=true');
+    if (isOutputEnabled) {
+      if (['plugins:update', 'plugins:update:check', 'update'].includes(options.Command.id)) {
+        await this.config.runHook('updateNotifier', {
+          updateCheckInterval: 0,
+          spawnOptions: { detached: false, stdio: 'ignore' },
+          defer: false,
+          changeLogUrl: {
+            'sfdx-cli': 'https://github.com/forcedotcom/cli/blob/main/releasenotes/README.md',
+            salesforcedx: 'https://github.com/forcedotcom/cli/blob/main/releasenotes/README.md',
+            'salesforce-alm': 'https://github.com/forcedotcom/cli/blob/main/releasenotes/README.md',
+          },
+        });
+        return;
+      }
       await this.config.runHook('updateNotifier', {
-        updateCheckInterval: 0,
-        spawnOptions: { detached: false, stdio: 'ignore' },
-        defer: false,
+        updateCheckInterval: 1000 * 60 * 60 * 24,
+        spawnOptions: { detached: true, stdio: 'ignore' },
+        defer: true,
         changeLogUrl: {
           'sfdx-cli': 'https://github.com/forcedotcom/cli/blob/main/releasenotes/README.md',
           salesforcedx: 'https://github.com/forcedotcom/cli/blob/main/releasenotes/README.md',
           'salesforce-alm': 'https://github.com/forcedotcom/cli/blob/main/releasenotes/README.md',
         },
       });
-      return;
     }
-    await this.config.runHook('updateNotifier', {
-      updateCheckInterval: 1000 * 60 * 60 * 24,
-      spawnOptions: { detached: true, stdio: 'ignore' },
-      defer: true,
-      changeLogUrl: {
-        'sfdx-cli': 'https://github.com/forcedotcom/cli/blob/main/releasenotes/README.md',
-        salesforcedx: 'https://github.com/forcedotcom/cli/blob/main/releasenotes/README.md',
-        'salesforce-alm': 'https://github.com/forcedotcom/cli/blob/main/releasenotes/README.md',
-      },
-    });
   }
 };
